303fbfbc6543549ac85d3a0c719e75c303607749
shivam182004 <shivamjoshiji2004@gmail.com>
2024-01-02 15:03:50 +0530

    small changes

--- script.js
+++ script.js
@@ -3,33 +3,48 @@ var tl = gsap.timeline();
 
 tl.from("#loadtext h1",{
     
-
+  
     y: 100,
-  ease: "power4.out",
-  delay: 1,
+   // ease: "power3.out",
+  ease:'bounce',
+  delay: 1.3,
   skewY: 7,
   stagger: {
     amount: 0.3
+  },
+  fontFamily: 'Dancing Script',
+  scrollTrigger:{
+    trigger:"#main",
+    scroller:"body",
+    scrub:2
   }
 })
 
 
 
 tl.to("#loaderpage",{
+
 top : "-100vh",
 dealy : 1,
 duration : 1.5
 });
-tl.to("#loadtext h1",{
-    
 
-    y: 100,
-  ease: "power4.out",
-  delay: 1,
-  skewY: 7,
-  stagger: {
-    amount: 0.3
-  }
+tl.from("#miantext h2",{
+  x:100,
+  duration:1,
+  delay:1,
+  stagger:1
+})
+
+
+tl.from("#contact h2", {
+  y: '100%',
+  opacity: 0,
+  duration: 0.5,
+  ease: 'power3.out',
+  stagger: 0.1,
+  
+ 
 })
 
 
@@ -47,9 +62,18 @@ Shery.mouseFollower({
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
-  Shery.makeMagnet("#home " /* Element to target.*/, {
+  Shery.makeMagnet("#maintext " /* Element to target.*/, {
     //Parameters are optional.
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
+    
   });
+  Shery.makeMagnet("#contact h3 " /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+    
+  });
+
+ 
  
